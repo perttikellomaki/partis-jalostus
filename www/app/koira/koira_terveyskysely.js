be2cbcf0590880cdc50882af2190a0c66e953e20
Pertti Kellomäki <pertti.kellomaki@iki.fi>
2014-03-18 12:51:47 +0200

    Add user name in health survey.

--- www/app/koira/koira_terveyskysely.js
+++ www/app/koira/koira_terveyskysely.js
@@ -1,8 +1,13 @@
-function KoiraTerveyskyselyCtrl ($scope, $routeParams, KoiraService, TerveyskyselyService, SidepanelService) {
+function KoiraTerveyskyselyCtrl ($scope, $routeParams, KoiraService, TerveyskyselyService, SidepanelService, LoginService) {
     SidepanelService.get().selection = 'terveyskysely';
     $scope.koira = KoiraService.get({uri: "/Koira/" + $routeParams.key});
     $scope.kysely = TerveyskyselyService.makeNew();
     $scope.kyselyt = [];
+    $scope.createNew = function () {
+	$scope.kysely.vastaaja = LoginService.nick();
+	$scope.kysely.koira = $scope.koira.virallinen_nimi;
+	$scope.create_new = true;
+    }
     $scope.send = function () {
 	$scope.kysely.timestamp = new Date();  // fixme: remove once saved on server
 	$scope.create_new = false;
@@ -10,5 +15,5 @@ function KoiraTerveyskyselyCtrl ($scope, $routeParams, KoiraService, Terveyskyse
 	$scope.kysely = TerveyskyselyService.makeNew();
     }
 }
-KoiraTerveyskyselyCtrl.$inject = ['$scope', '$routeParams', 'KoiraService', 'TerveyskyselyService', 'SidepanelService'];
+KoiraTerveyskyselyCtrl.$inject = ['$scope', '$routeParams', 'KoiraService', 'TerveyskyselyService', 'SidepanelService', 'LoginService'];
 
